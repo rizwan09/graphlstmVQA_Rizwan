997b09b6c12bb302fedc0fe423ac042aa19d9809
rizwan09 <rizwan.incipient@gmail.com>
2017-02-03 01:22:33 -0500

    find [y,y,n,y,n,y,y,y]

---
+++ debuging_train_ans.py
@@ -0,0 +1,22 @@
+import json
+matching_ans = ['yes', 'yes', 'no', 'yes', 'no', 'yes', 'yes', 'yes']
+
+with open('vqa_data/train_all_ans.txt', "r") as train_annoattion_f:
+    m_index = 0
+    id = 0
+    for line in train_annoattion_f:
+        ans = line.strip()
+        #print "ans: ", ans, " id: ", id, " mIndex: ", m_index
+        if(m_index==matching_ans.__len__()-1 ):
+            print "found at ended: ", id
+            break
+        if(ans == matching_ans[m_index]):
+            m_index += 1
+        else:
+            m_index = 0
+
+
+
+        id +=1
+
+
