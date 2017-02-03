3ea3370126c97458e761f76eaa94e37c9e7c3adc
rizwan09 <rizwan.incipient@gmail.com>
2017-02-03 01:32:23 -0500

    find [y,y,n,y,n,y,y,y]

--- debuging_train_ans.py
+++ debuging_train_ans.py
@@ -7,8 +7,8 @@ with open('vqa_data/train_all_ans.txt', "r") as train_annoattion_f:
     for line in train_annoattion_f:
         ans = line.strip()
         #print "ans: ", ans, " id: ", id, " mIndex: ", m_index
-        if(m_index==matching_ans.__len__()-1 ):
-            print "found at ended: ", id
+        if(m_index==matching_ans.__len__()):
+            print "found at ended: ", id, m_index
             break
         if(ans == matching_ans[m_index]):
             m_index += 1
